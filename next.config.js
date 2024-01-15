dfcf54a7e7c8515a076e2e589c05d2f6fb395ec3
Folakunmi Aremu <folakunmiaremu@Folakunmis-MacBook-Pro.local>
2024-01-15 20:04:41 +0100

    first push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')();
-const nextConfig = {}
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+}
 
 module.exports = nextConfig
 

d9cd2bb79190423acdfd88e9054121123426e4bc
Folakunmi Aremu <folakunmiaremu@Folakunmis-MacBook-Pro.local>
2024-01-16 15:58:33 +0100

    update fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Image from "next/image";
 import { useAuth } from "../context/AuthContext";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const { loggedinUser }: any = useAuth();
   console.log(loggedinUser);
+  redirect(`/app/dashboard`);
   return (
     <div>
       <p>Hello World</p>
